fedf0e1c4b9ce3140dbb171692c1ffa21a5affe9
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2019-05-06 02:58:35 +0000

    working skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import express = require('express');
 
-console.log('working')
+var app = express();
+
+
+
+app.use((req, res) => {
+    res.status(200).send('Hello world')
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('App is listening for new connections')
+})
+
