27ab499e913f831c6d3eeee66e2fb53208d26bfa
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2019-05-06 02:25:23 +0000

    initial setup

---
+++ scripts/migrations/initial/0.diagnostic_codes.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.dignostic_codes(
+    id serial PRIMARY KEY,
+    category_name text NOT NULL,
+    icd9_code text UNIQUE NOT NULL,
+    icd10_code text UNIQUE NOT NULL, 
+    short_desc text NOT NULL,
+    full_desc text NOT NULL
+);
