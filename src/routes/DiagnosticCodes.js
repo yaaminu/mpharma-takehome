bef2ba00054411caebc80fe554241bcca5934822
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2019-05-07 14:09:01 +0000

    remove a dangling javscript file generated by tsc

--- src/routes/DiagnosticCodes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1.Router();
-router.post('/diagnostic_codes', function (req, res, next) {
-    res.end('working');
-});
-exports["default"] = router;
